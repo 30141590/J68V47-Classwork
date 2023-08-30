a72d9037fcf12e7e06e56d7888300e91963ddc4b
30141590 <30141590@nescol.ac.uk>
2023-08-30 12:00:31 +0100

    Lesson 1

---
+++ src/Lesson1Exercise2_Anagrams.java
@@ -0,0 +1,10 @@
+public class Lesson1Exercise2_Anagrams {
+    public static void main  (String[] args) {
+        System.out.println("Here are five, nine letter anagrams:");
+        System.out.println("Burt Lefty = Butterfly");
+        System.out.println("Afro Tonne = Afternoon");
+        System.out.println("Dart Venue = Adventure");
+        System.out.println("Adore Snug = Dangerous");
+        System.out.println("And Places = Landscape");
+    }
+}

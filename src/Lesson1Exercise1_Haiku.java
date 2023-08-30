259b05caa6d6c9869a160587bc54ccf6e57576a5
30141590 <30141590@nescol.ac.uk>
2023-08-30 11:41:57 +0100

    First Commit

---
+++ src/Lesson1Exercise1_Haiku.java
@@ -0,0 +1,7 @@
+public class Lesson1Exercise1_Haiku {
+    public static void main  (String[] args) {
+        System.out.println("Whispers of the breeze,");
+        System.out.println("Nature's secrets gently shared,");
+        System.out.println("Peace blooms in each leaf.");
+    }
+}

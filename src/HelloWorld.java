e9ec18f798bc6fe1cd27ed78360703eaf79a04e2
30141590 <30141590@nescol.ac.uk>
2023-08-30 11:09:36 +0100

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * My First Program
+ * Callum Taylor
+ * Version 1
+ */
+
+ class HelloWorld {
+
+    public static void main  (String[] args) {
+        System.out.println("Hello World");
+    }
+}

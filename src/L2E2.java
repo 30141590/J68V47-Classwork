1a4b9a7ce1af46439ace4e164ae5564f9c10e829
30141590 <30141590@nescol.ac.uk>
2023-09-06 11:20:43 +0100

    Lesson 1

---
+++ src/L2E2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class L2E2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("choose a whole number between 1 and 10: ");
+        int FirstNum = input.nextInt();
+
+        System.out.print("choose another whole number between 1 and 10: ");
+        int SecNum = input.nextInt();
+        System.out.println(" ");
+        double Exponent = Math.pow(FirstNum,SecNum);
+        int TwoD = (int) Exponent;
+
+        System.out.println(FirstNum + "+" + SecNum + " = " + (FirstNum+SecNum));
+        System.out.println(FirstNum + "-" + SecNum + " = " + (FirstNum-SecNum));
+        System.out.println(FirstNum + "x" + SecNum + " = " + (FirstNum*SecNum));
+        System.out.println(FirstNum + "÷" + SecNum + " = " + (FirstNum/SecNum));
+        System.out.println(FirstNum + "^" + SecNum + " = " + TwoD);
+
+    }
+}

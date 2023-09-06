04305907260c6e3aa6c1bdd266dddb5142fd6bea
30141590 <30141590@nescol.ac.uk>
2023-09-06 10:20:45 +0100

    Lesson 1

---
+++ src/L2E1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class L2E1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+        System.out.println("Your current name is: " + name);
+
+        System.out.print("What is your desired name? ");
+        name = input.nextLine();
+        System.out.print("your new name is: " + name);
+    }
+}
---
+++ src/L2E1Real.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class L2E1Real {
+
+    public static void main(String[] args) {
+
+
+    }
+}

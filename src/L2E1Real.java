7228ced285f96bb1cff2ce8c596df7e22d1d954f
30141590 <30141590@nescol.ac.uk>
2023-09-06 10:32:22 +0100

    Lesson 1

--- src/L2E1Real.java
+++ src/L2E1Real.java
@@ -2,6 +2,17 @@ import java.util.*;
 public class L2E1Real {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+
+        System.out.print("What is your favourite hobby? ");
+        String hobby = input.nextLine();
+
+        System.out.println(name + " loves " + hobby + ".");
+        System.out.println("They really enjoy " + hobby + ", they like to do it as often as possible.");
+        System.out.println(name + " finds " + hobby + " relaxing and it helps them pass some time.");
+
 
 
     }

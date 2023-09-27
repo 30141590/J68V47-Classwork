70b1df5faf0adc7b92d00f9bbd2ea5acc486e41c
30141590 <30141590@nescol.ac.uk>
2023-09-27 09:59:33 +0100

    Lesson 1

---
+++ src/Lesson5/L5E2.java
@@ -0,0 +1,17 @@
+package Lesson5;
+import java.util.*;
+public class L5E2 {
+    public static void main(String[] args) {
+
+        for( int count=11; count>=1; count--)
+        {
+            if (count >1){
+                System.out.println(count-1);
+            } else {
+                System.out.println("blastoff !!!!!");
+            }
+
+        }
+
+    }
+}

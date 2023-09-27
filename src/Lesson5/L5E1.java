1b72887216f7e1c45ba14f0127e5254271a700e6
30141590 <30141590@nescol.ac.uk>
2023-09-27 09:40:02 +0100

    Lesson 1

---
+++ src/Lesson5/L5E1.java
@@ -0,0 +1,14 @@
+package Lesson5;
+import java.util.*;
+
+public class L5E1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What numbers times table would you like to see: ");
+        int Number = input.nextInt();
+        for( int count=0; count<=12; count++ ){
+            System.out.print(Number*count + ", ");
+
+        }
+    }
+}

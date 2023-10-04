9bbe032a8e334d3d7a4fea0f7b75c88eac142cb0
30141590 <30141590@nescol.ac.uk>
2023-10-04 09:53:19 +0100

    Lesson 1

---
+++ src/Lesson6/L6E1.java
@@ -0,0 +1,13 @@
+package Lesson6;
+public class L6E1 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count <= 24){
+            count++;
+            if (count % 2 == 0){
+                System.out.println(count);
+            }
+        }
+
+    }
+}

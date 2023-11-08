fa32c2d6be346da6781188dd3fecddfe347dbde6
30141590 <30141590@nescol.ac.uk>
2023-11-08 13:59:45 +0000

    L8

---
+++ src/Lesson8/L8E1.java
@@ -0,0 +1,13 @@
+package Lesson8;
+import java.util.*;
+public class L8E1 {
+    public static void print(){
+        String[] classmates = {"Noah", "Oliver", "Cameron", "Connor", "Luke"};
+        System.out.println(classmates[0]);
+        System.out.println(classmates[4]);
+        System.out.println(classmates.length);
+    }
+    public static void main(String[] args) {
+        print();
+    }
+}

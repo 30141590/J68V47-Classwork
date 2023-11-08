ff666edcd1c0c79c18be54ec2553c9f69816e735
30141590 <30141590@nescol.ac.uk>
2023-11-08 14:02:11 +0000

    L8

--- src/Lesson8/L8E1.java
+++ src/Lesson8/L8E1.java
@@ -4,8 +4,8 @@ public class L8E1 {
     public static void print(){
         String[] classmates = {"Noah", "Oliver", "Cameron", "Connor", "Luke"};
         System.out.println(classmates[0]);
-        System.out.println(classmates[4]);
-        System.out.println(classmates.length);
+        System.out.println(classmates[classmates.length-1]);
+        System.out.format("The number of people in the class is: %d",classmates.length);
     }
     public static void main(String[] args) {
         print();
---
+++ src/Lesson8/L8E2.java
@@ -0,0 +1,4 @@
+package Lesson8;
+
+public class L8E2 {
+}

0e62854272e4be616860bd23ec064fd99b1eb7a5
30141590 <30141590@nescol.ac.uk>
2023-11-14 13:43:20 +0000

    idk

--- src/Lesson9/L9E2.java
+++ src/Lesson9/L9E2.java
@@ -14,16 +14,16 @@ public class L9E2 {
     }
     public static void exception2() {
         // exception 2
-        // java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
+        // no error
         try {
             int exception2 = Integer.MAX_VALUE + 1;
-        } catch (Exception e) {
+        }catch (Exception e){
             System.out.println(e.toString());
         }
     }
     public static void exception3() {
         // exception 3
-        // java.lang.NullPointerException: Cannot load from int array because "myArray4" is null
+        // java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
         try {
             int[] myArray3 = new int[5];
             int exception3 = myArray3[5];
@@ -33,7 +33,7 @@ public class L9E2 {
     }
     public static void exception4() {
         // exception 4
-        // java.io.FileNotFoundException: exception5.txt (The system cannot find the file specified)
+        // java.lang.NullPointerException: Cannot load from int array because "myArray4" is null
         try {
             int[] myArray4 = null;
             int exception4 = myArray4[0];
@@ -43,7 +43,7 @@ public class L9E2 {
     }
     public static void exception5() {
         // exception 5
-        // java.lang.NumberFormatException: For input string: "Hello"
+        // java.io.FileNotFoundException: exception5.txt (The system cannot find the file specified)
         try {
             File exception5 = new File("exception5.txt");
             Scanner scanner = new Scanner(exception5);
@@ -53,7 +53,7 @@ public class L9E2 {
     }
     public static void exception6() {
         // exception 6
-        // java.lang.NullPointerException: Cannot invoke "String.toUpperCase()" because "exception7" is null
+        // java.lang.NumberFormatException: For input string: "Hello"
         try {
             String thisString = "Hello";
             int exception6 = Integer.parseInt(thisString);
@@ -63,6 +63,7 @@ public class L9E2 {
     }
     public static void exception7() {
         // exception 7
+        // java.lang.NullPointerException: Cannot invoke "String.toUpperCase()" because "exception7" is null
         try {
             String exception7 = null;
             System.out.println(exception7.toUpperCase());
